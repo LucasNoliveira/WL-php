23bcda37f940f28e105bc0b2f26b60ebce544acc
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-01-29 16:34:57 -0300

    toggle patient registering exam registering

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,52 @@
-// src/index.js
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-// import ApiTest from './ApiTest';
 import Form from './components/Form';
 import ExamForm from './components/ExamForm';
 
+const App = () => {
+  const [showPatientForm, setShowPatientForm] = useState(true);
+  const [showExamForm, setShowExamForm] = useState(false);
+
+  const handleExamLinkClick = () => {
+    setShowPatientForm(false);
+    setShowExamForm(true);
+  };
+
+  const handlePatientLinkClick = () => {
+    setShowExamForm(false);
+    setShowPatientForm(true);
+  };
+
+  return (
+    <div>
+      {showPatientForm && (
+        <div>
+          <h1>Patient Registration</h1>
+          <Form />
+        </div>
+      )}
+
+      {showExamForm && (
+        <div>
+          <h1>Exam Registration</h1>
+          <ExamForm />
+        </div>
+      )}
+
+      {!showExamForm && (
+        <button onClick={handleExamLinkClick}>Click here to register an exam</button>
+      )}
+
+      {!showPatientForm && (
+        <button onClick={handlePatientLinkClick}>Click here to register a patient</button>
+      )}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Form  />
-    <ExamForm  />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
