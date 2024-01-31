685a64c0316eafa9de8a21f173d4b16d49802aba
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-01-30 23:34:14 -0300

    campos obrigatórios form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,204 +1,225 @@
 import React, { useState, useEffect } from 'react';
 import {
-    TextField,
-    Button,
-    Container,
-    Typography,
-    Grid,
-    Select,
-    MenuItem,
-    InputLabel,
-    FormControl,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    List,
-    ListItem,
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Grid,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  List,
+  ListItem,
 } from '@mui/material';
 
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({
-        nome_completo: '',
-        sexo: '',
-        email: '',
-        celular: '',
-        exames: [],
-    });
+  const [formData, setFormData] = useState({
+    nome_completo: '',
+    sexo: '',
+    email: '',
+    celular: '',
+    exames: [],
+  });
 
-    const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
-    const [patientData, setPatientData] = useState(null);
-    const [exames, setExames] = useState([]);
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
+  const [patientData, setPatientData] = useState(null);
+  const [exames, setExames] = useState([]);
+  const [errorFields, setErrorFields] = useState([]);
 
-    useEffect(() => {
-        const fetchExames = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:8000/api/exames');
-                if (response.ok) {
-                    const dadosExames = await response.json();
-                    setExames(dadosExames);
-                    console.log(dadosExames);
-                } else {
-                    console.error('Falha ao buscar os exames');
-                }
-            } catch (error) {
-                console.error('Erro ao buscar os exames:', error);
-            }
-        };
-
-        fetchExames();
-    }, []);
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((dadosAnteriores) => ({
-            ...dadosAnteriores,
-            [name]: value,
-        }));
-    };
-
-    const handleExameChange = (selectedExames) => {
-        setFormData((dadosAnteriores) => ({
-            ...dadosAnteriores,
-            exames: selectedExames,
-        }));
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await fetch('http://127.0.0.1:8000/api/cadastrar-paciente', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-
-            if (response.ok) {
-                const dadosResposta = await response.json();
-                console.log('Paciente cadastrado com sucesso:', dadosResposta);
-
-                const { numero_atendimento } = dadosResposta;
-                const respostaPaciente = await fetch(`http://127.0.0.1:8000/api/pacientes/${numero_atendimento}`);
-                const dadosPaciente = await respostaPaciente.json();
-                setPatientData(dadosPaciente);
-            } else {
-                console.error('Falha ao cadastrar o paciente');
-            }
-        } catch (error) {
-            console.error('Erro:', error);
+  useEffect(() => {
+    const fetchExames = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/exames');
+        if (response.ok) {
+          const dadosExames = await response.json();
+          setExames(dadosExames);
+          console.log(dadosExames);
+        } else {
+          console.error('Falha ao buscar os exames');
         }
+      } catch (error) {
+        console.error('Erro ao buscar os exames:', error);
+      }
     };
 
-    const handleWelcomeMessageClose = () => {
-        setShowWelcomeMessage(false);
-    };
+    fetchExames();
+  }, []);
 
-    return (
-        <Container maxWidth="sm">
-            <Dialog open={showWelcomeMessage} onClose={handleWelcomeMessageClose}>
-                <DialogTitle>Bem-vindo admin</DialogTitle>
-                <DialogContent>
-                    <Typography>
-                        Você está no perfil de administrador. Você tem todas as permissões para criar pacientes, registros e puxar relatórios
-                    </Typography>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleWelcomeMessageClose} color="primary">
-                        Ok
-                    </Button>
-                </DialogActions>
-            </Dialog>
-            {patientData ? (
-                <div style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '16px', borderRadius: '8px', background: '#fff' }}>
-                    <Typography variant="h5">Relatório do Paciente</Typography>
-                    <Typography>Nome: {patientData.nome_completo}</Typography>
-                    <Typography>Gênero: {patientData.sexo}</Typography>
-                    <Typography>Email: {patientData.email}</Typography>
-                    <Typography>Celular: {patientData.celular}</Typography>
-                    <Typography variant="h6" style={{ marginTop: '16px' }}>Exames:</Typography>
-                    <List>
-                        {patientData.exames.map((exame) => (
-                            <ListItem key={exame.codigo} style={{ padding: '8px', borderBottom: '1px solid #ccc' }}>
-                                <Typography>{exame.descricao}</Typography>
-                            </ListItem>
-                        ))}
-                    </List>
-                </div>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="Nome Completo"
-                                name="nome_completo"
-                                value={formData.nome_completo}
-                                onChange={handleInputChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControl fullWidth>
-                                <InputLabel>Gênero</InputLabel>
-                                <Select
-                                    name="sexo"
-                                    value={formData.sexo}
-                                    onChange={handleInputChange}
-                                    label="Gênero"
-                                >
-                                    <MenuItem value="">Selecione o seu Gênero</MenuItem>
-                                    <MenuItem value="M">Masculino</MenuItem>
-                                    <MenuItem value="F">Feminino</MenuItem>
-                                    <MenuItem value="O">Outro</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="Email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                label="Celular"
-                                name="celular"
-                                value={formData.celular}
-                                onChange={handleInputChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControl fullWidth>
-                                <InputLabel>Exames</InputLabel>
-                                <Select
-                                    name="exames"
-                                    value={formData.exames}
-                                    onChange={(e) => handleExameChange(e.target.value)}
-                                    label="Exames"
-                                    multiple
-                                >
-                                    {exames.map((exame) => (
-                                        <MenuItem key={exame.codigo} value={exame.codigo}>
-                                            {exame.descricao}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                    </Grid>
-                    <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>
-                        Cadastrar paciente
-                    </Button>
-                </form>
-            )}
-        </Container>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((dadosAnteriores) => ({
+      ...dadosAnteriores,
+      [name]: value,
+    }));
+    setErrorFields((prevErrorFields) => prevErrorFields.filter((field) => field !== name));
+  };
+
+  const handleExameChange = (selectedExames) => {
+    setFormData((dadosAnteriores) => ({
+      ...dadosAnteriores,
+      exames: selectedExames,
+    }));
+    setErrorFields((prevErrorFields) => prevErrorFields.filter((field) => field !== 'exames'));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Verificar campos obrigatórios
+    const requiredFields = ['nome_completo', 'sexo', 'exames'];
+    const emptyFields = requiredFields.filter((field) => {
+      const value = formData[field];
+      return (typeof value === 'string' && !value.trim()) || (Array.isArray(value) && value.length === 0);
+    });
+    
+    if (emptyFields.length > 0) {
+      setErrorFields(emptyFields);
+      return;
+    }
+    
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/cadastrar-paciente', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const dadosResposta = await response.json();
+        console.log('Paciente cadastrado com sucesso:', dadosResposta);
+
+        const { numero_atendimento } = dadosResposta;
+        const respostaPaciente = await fetch(`http://127.0.0.1:8000/api/pacientes/${numero_atendimento}`);
+        const dadosPaciente = await respostaPaciente.json();
+        setPatientData(dadosPaciente);
+      } else {
+        console.error('Falha ao cadastrar o paciente');
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
+
+  const handleWelcomeMessageClose = () => {
+    setShowWelcomeMessage(false);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Dialog open={showWelcomeMessage} onClose={handleWelcomeMessageClose}>
+        <DialogTitle>Bem-vindo admin</DialogTitle>
+        <DialogContent>
+          <Typography>
+            Você está no perfil de administrador. Você tem todas as permissões para criar pacientes, registros e puxar relatórios
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleWelcomeMessageClose} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {patientData ? (
+        <div style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '16px', borderRadius: '8px', background: '#fff' }}>
+          <Typography variant="h5">Relatório do Paciente</Typography>
+          <Typography>Nome: {patientData.nome_completo}</Typography>
+          <Typography>Gênero: {patientData.sexo}</Typography>
+          <Typography>Email: {patientData.email}</Typography>
+          <Typography>Celular: {patientData.celular}</Typography>
+          <Typography variant="h6" style={{ marginTop: '16px' }}>Exames:</Typography>
+          <List>
+            {patientData.exames.map((exame) => (
+              <ListItem key={exame.codigo} style={{ padding: '8px', borderBottom: '1px solid #ccc' }}>
+                <Typography>{exame.descricao}</Typography>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Nome Completo"
+                name="nome_completo"
+                value={formData.nome_completo}
+                onChange={handleInputChange}
+                error={errorFields.includes('nome_completo')}
+                helperText={errorFields.includes('nome_completo') && 'Campo obrigatório'}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel>Gênero</InputLabel>
+                <Select
+                  name="sexo"
+                  value={formData.sexo}
+                  onChange={handleInputChange}
+                  label="Gênero"
+                  error={errorFields.includes('sexo')}
+                >
+                  <MenuItem value="">Selecione o seu Gênero</MenuItem>
+                  <MenuItem value="M">Masculino</MenuItem>
+                  <MenuItem value="F">Feminino</MenuItem>
+                  <MenuItem value="O">Outro</MenuItem>
+                </Select>
+                {errorFields.includes('sexo') && <Typography style={{ color: 'red' }}>Campo obrigatório</Typography>}
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Celular"
+                name="celular"
+                value={formData.celular}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel>Exames</InputLabel>
+                <Select
+                  name="exames"
+                  value={formData.exames}
+                  onChange={(e) => handleExameChange(e.target.value)}
+                  label="Exames"
+                  multiple
+                  error={errorFields.includes('exames')}
+                >
+                  {exames.map((exame) => (
+                    <MenuItem key={exame.codigo} value={exame.codigo}>
+                      {exame.descricao}
+                    </MenuItem>
+                  ))}
+                </Select>
+                {errorFields.includes('exames') && <Typography style={{ color: 'red' }}>Campo obrigatório</Typography>}
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>
+            Cadastrar paciente
+          </Button>
+        </form>
+      )}
+    </Container>
+  );
 };
 
 export default RegistrationForm;
